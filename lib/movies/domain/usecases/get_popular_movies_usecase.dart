af39c6e4b8ba985f52fd20dcf98aeb00e644ac47
MohamedElsafty <me5574871@gmail.com>
2025-10-19 02:42:34 +0300

    repo

--- lib/movies/domain/usecases/get_popular_movies_usecase.dart
+++ lib/movies/domain/usecases/get_popular_movies_usecase.dart
@@ -1,9 +1,12 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../core/error/failure.dart';
 import '../entities/movie.dart';
 import '../repository/base_movies_repository.dart';
 
 class GetPopularMoviesUseCase {
   final BaseMoviesRepository baseMoviesRepository;
   GetPopularMoviesUseCase(this.baseMoviesRepository);
-  Future<List<Movie>> execute() async {
+  Future<Either<Failure, List<Movie>>> execute() async {
     return await baseMoviesRepository.getPopularMovies();}
 }
